2b74e6c74d966eea187ead7166c181adeff063d0
arda <arda150504@gmail.com>
2024-10-30 02:22:24 +0700

    menambahkan fitur baru

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+/**
+ * ini adalah method main dimana untuk menjalankan suatu program
+ *
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("jelita elekkkkkkk");
+        System.out.println("wkwkwkwkwkwkwkwkwkwkwkkkwkwkk");
     }
 }
